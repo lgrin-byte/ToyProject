9dc83b7a0bbbe87880784a584b1dd9c241ec80ca
lgrin-byte <lgrin02@naver.com>
2023-01-29 14:24:42 +0900

    Feat : 결과 페이지 마크업

---
+++ src/pages/result/resultStyle.jsx
@@ -0,0 +1,33 @@
+import styled, { keyframes } from 'styled-components';
+import Link from "next/link"
+
+export const ChoiceYear = styled.h2`
+    text-align: center;
+    margin-bottom: 38px;
+`;
+
+export const P = styled.p`
+    text-align: center;
+    font-size: 16px;
+    margin:25px 0 50px;
+    color: red;
+`;
+
+
+export const Wrap = styled.div`
+    width: 329px;
+    text-align: center;
+
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto ;
+    padding-top: 3rem;
+    
+`;
+
+export const Image = styled.div`
+height: 350px;
+margin: 36px 0 35px;
+background-color: beige;
+
+`
