4daeb6479661788c365d86d768a9747bff849e6f
lgrin-byte <lgrin02@naver.com>
2023-03-05 23:42:58 +0900

    Feat : 리덕스 툴킷 수정

--- src/hooks/count/counterSlice.js
+++ src/hooks/count/counterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const userSlice = createSlice({
     name: "user",
-    initialState: { value: {name: "", year: "", music:[] ,musicImg:"", score: ""}},
+    initialState: { value: {name: "", year: "", music:[] ,musicImg:"", score: "",state:0}},
     reducers: {
         login: (state, action) => {
             state.value = action.payload
