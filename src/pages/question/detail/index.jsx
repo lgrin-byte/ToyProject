450f6972f12066c626380dd71c0ccf2245769430
lgrin-byte <lgrin02@naver.com>
2023-03-11 21:21:22 +0900

    Feat : 새로고침 이벤트 추가

--- src/pages/question/detail/index.jsx
+++ src/pages/question/detail/index.jsx
@@ -192,21 +192,21 @@ useEffect(()=>{
         setTitle("");
     }, [level]);
     // // 새로고침 막기 변수
-    // const preventClose = (e) => {
-    //     e.preventDefault();
-    //     e.returnValue = ""; // chrome에서는 설정이 필요해서 넣은 코드
-    // };
+    const preventClose = (e) => {
+        e.preventDefault();
+        e.returnValue = ""; // chrome에서는 설정이 필요해서 넣은 코드
+    };
 
     // // 브라우저에 렌더링 시 한 번만 실행하는 코드
-    // useEffect(() => {
-    //     (() => {
-    //         window.addEventListener("beforeunload", preventClose);
-    //     })();
+    useEffect(() => {
+        (() => {
+            window.addEventListener("beforeunload", preventClose);
+        })();
 
-    //     return () => {
-    //         window.removeEventListener("beforeunload", preventClose);
-    //     };
-    // }, []);
+        return () => {
+            window.removeEventListener("beforeunload", preventClose);
+        };
+    }, []);
 
     const onKeyPress = (e) => {
         if (e.key === "Enter") {
