72eab21f340938308db7bc40efedf10a868bcaae
lgrin-byte <lgrin02@naver.com>
2023-03-13 15:14:40 +0900

    Docs : removed cached

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./hooks/count/counterSlice";
-
-export default configureStore({
-    reducer: {
-        user: userReducer,
-    },
-});
